6f5cc3754ccc7eb7b65e1fc4497fd245a4cb70b3
Tiastlii <380747287@qq.com>
2022-07-05 00:33:32 +0200

    add opcua disconnect

--- opc_2_bbb_bridge.py
+++ opc_2_bbb_bridge.py
@@ -40,7 +40,10 @@ async def opc_get_all_node_value(client: Client) -> dict:
 
     return dic 
     
-
+# disconnect the opcua server
+async def opc_disconnect(client: Client):
+    assert client is not None, "opcua client is None"
+    await client.disconnect()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARN)
